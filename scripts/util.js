2e2b58087ab1b4c46898e41800b0414a4d5fb5e7
joe <josephweller@protonmail.com>
2021-07-28 23:27:59 +0100

    added methods

--- scripts/util.js
+++ scripts/util.js
@@ -1,26 +1,27 @@
-const getFloatFromElementId = function(nodeId) {
-    let result = Number.parseFloat(
-        getValueByElementId(nodeId)
-    )
-    if (result === NaN) {
-        setElementInnerHtmlById("Error", `${nodeId} is not a valid number`)
-        result = 0;
-    }
-    return result;
-}
-
 const getNode = function(nodeId) {
     return document.getElementById(nodeId)
+}
+
+const getFloatFromElementId = function(nodeId) {
+    return Number.parseFloat(getValueByElementId(nodeId)) || 0
+}
+
+const getCurrencyFromElementId = function(nodeId) {
+    return Number.parseFloat(getValueByElementId(nodeId) * 100) || 0
+}
+
+const convertToDecimal = function(float) {
+    return (float / 100).toFixed(2)
+}
+
+const getInnerHtmlFromElementId = function(nodeId) {
+    return document.getElementById(nodeId).innerHTML
 }
 
 const getIntFromElementId = function(nodeId) {
     let result = Number.parseInt(
         getValueByElementId(nodeId)
     )
-    if (result === NaN) {
-        setElementInnerHtmlById("Error", `${nodeId} is not a valid number`)
-        result = 0;
-    }
     return result;
 }
 
